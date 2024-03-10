146282b30190e3c1cae20ac1b979fb595d9de0e2
mansarawa <you@example.com>
2024-03-10 06:36:07 +0530

    responsice done

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -20,6 +20,7 @@ function Navbar() {
           <img src={logo} className={Nav.logo} alt="" srcSet="" />
         </div>
         <div className={Nav.menus}>
+          <div className={Nav.def}>
           <ul className={Nav.ul}>
             <li className={Nav.li}><Link to='/' className={Nav.item}>Home</Link></li>
             <li className={Nav.li}><Link to='/' className={Nav.item}>Products</Link></li>
@@ -27,24 +28,26 @@ function Navbar() {
             <li className={Nav.li}><Link to='/register' className={Nav.item}>Register</Link></li>
             <li className={Nav.li}><Link to='/login' className={Nav.item}>Login</Link></li>
           </ul>
-          {/* Responsive menu */}
-          {resp &&
-            <div className={Nav.response}>
-              <ul className={Nav.ul}>
-                <li className={Nav.li}><Link to='/' className={Nav.item}>Home</Link></li>
-                <li className={Nav.li}><Link to='/' className={Nav.item}>Products</Link></li>
-                <li className={Nav.li}><Link to='/' style={{ backgroundColor: '#201a44', color: 'white', padding: '5px 15px 5px 15px', borderRadius: '20px' }} className={Nav.item}>Cart</Link></li>
-                <li className={Nav.li}><Link to='/register' className={Nav.item}>Register</Link></li>
-                <li className={Nav.li}><Link to='/login' className={Nav.item}>Login</Link></li>
-              </ul>
-            </div>
-          }
+          </div>
+          
           <div className={Nav.burger} onClick={hamclick}>
-            {/* Hamburger menu icon */}
+           
             <RiMenu4Fill />
           </div>
         </div>
+       
       </div>
+      {resp &&
+            <div className={Nav.response}>
+              <ul className={Nav.ul} style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+                <li className={Nav.li} ><Link to='/' className={Nav.item}>Home</Link></li>
+                <li className={Nav.li} style={{marginTop:'1%'}}><Link to='/' className={Nav.item}>Products</Link></li>
+                <li className={Nav.li} style={{marginTop:'2%'}}><Link to='/' style={{ backgroundColor: '#201a44', color: 'white', padding: '5px 15px 5px 15px', borderRadius: '20px' }} className={Nav.item}>Cart</Link></li>
+                <li className={Nav.li} style={{marginTop:'1%'}}><Link to='/register' className={Nav.item}>Register</Link></li>
+                <li className={Nav.li} style={{marginTop:'1%'}}><Link to='/login' className={Nav.item}>Login</Link></li>
+              </ul>
+            </div>
+          }
     </>
   )
 }
